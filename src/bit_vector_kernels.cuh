8aa7b709bd3e17af77bd4460b52bb5cf53b49f8e
Robin Kobus <kobus@uni-mainz.de>
2018-07-20 11:30:50 +0200

    initialize with 0 for small thresholds, additional bounds check in col kernel, tried double update per line

--- src/bit_vector_kernels.cuh
+++ src/bit_vector_kernels.cuh
@@ -24,12 +24,15 @@ void initFactor(bit_vector_t * Ab,
 
     if(tid < height) {
         fast_kiss_state32_t state = get_initial_fast_kiss_state32(seed + tid);
+
         const int randDepth = -log2f(threshold)+1;
-
-        bit_vector_t Ai = ~bit_vector_t(0) >> (32-factorDim);
-        for(int d=0; d<randDepth; ++d)
-            Ai &= fast_kiss32(state);
-
+        bit_vector_t Ai = 0;
+        // if threshold very small simply initilize as 0s (also catch threshold=0)
+        if(randDepth < 16) {
+            Ai = ~bit_vector_t(0) >> (32-factorDim);
+            for(int d=0; d<randDepth; ++d)
+                Ai &= fast_kiss32(state);
+        }
         Ab[tid] = Ai;
     }
 }
@@ -155,7 +158,7 @@ vectorMatrixMultCompareRowWarpShared(bit_factor_t *A,
                                      const float temperature,
                                      const float flipManyChance,
                                      const uint32_t flipManyDepth,
-                                     const int weight)
+                                     const error_t weight)
 {
     __shared__ bit_factor_t B_block[ 32 * WARPSPERBLOCK ];
     __shared__ bit_matrix_t C_block[ 32 * WARPSPERBLOCK ];
@@ -172,11 +175,13 @@ vectorMatrixMultCompareRowWarpShared(bit_factor_t *A,
     
     const bit_factor_t A_i = i < height ? A[i] : 0;
     bit_factor_t A_i_changed = 0;
+    // bit_factor_t A_i_changed2 = 0;
     // if (warpLane == 0 && i < height) {
     if (i < height) {
         state = get_initial_fast_kiss_state32(seed + warpId);
 
         A_i_changed = A_i ^ get_flip_mask(factorDim, state, flipManyChance, flipManyDepth);
+        // A_i_changed2 = A_i ^ get_flip_mask(factorDim, state, flipManyChance, flipManyDepth);
         // A_i_changed = get_flip_mask_many(factorDim, state, flipManyDepth);
     }
     // A_i_changed = __shfl_sync(FULLMASK, A_i_changed, 0);
@@ -187,6 +192,7 @@ vectorMatrixMultCompareRowWarpShared(bit_factor_t *A,
     const index_t col_in_tile = warpLane;
     const index_t padded_width_blocks = SDIV(width, WARPSPERBLOCK*32) * WARPSPERBLOCK*32;
     error_t error_thread = 0;
+    // error_t error_thread2 = 0;
     for (index_t j = threadIdx.x; j < padded_width_blocks; j += WARPSPERBLOCK*32) {
         B_block[threadIdx.x] = (j < width) ? B[j] : 0;
         C_block[threadIdx.x] = (j < width) ? C[vecFirst + j] : 0;
@@ -195,6 +201,8 @@ vectorMatrixMultCompareRowWarpShared(bit_factor_t *A,
         if(i < height) {
             #pragma unroll
             for(index_t w = 0; w < WARPSPERBLOCK; ++w) {
+                // const uint32_t dropout = fast_kiss32(state) | fast_kiss32(state) | fast_kiss32(state) & (1 << warpLane);
+                // const bit_factor_t B_j = dropout ? B_block[w*warpSize + warpLane] : 0;
                 const bit_factor_t B_j = B_block[w*warpSize + warpLane];
                 const int C_ij = (C_block[w*warpSize + col_in_tile] >> vecLane) & 1;
                 // index_t col = j / blockDim.x * blockDim.x + w*warpSize + warpLane;
@@ -202,6 +210,7 @@ vectorMatrixMultCompareRowWarpShared(bit_factor_t *A,
                 // int C_ij = (C[vecFirst + col] >> vecLane) & 1;
 
                 const int product_new = (B_j & A_i_changed) ? 1 : 0;
+                // const int product_new2 = (B_j & A_i_changed2) ? 1 : 0;
                 const int product_old = (B_j & A_i        ) ? 1 : 0;
 
                 // const float count_new = __popc(B_j & A_i_changed);
@@ -210,22 +219,34 @@ vectorMatrixMultCompareRowWarpShared(bit_factor_t *A,
                 // if (col < width)
                 error_thread += error_measure(product_new, C_ij, weight)
                               - error_measure(product_old, C_ij, weight);
+                // error_thread += 0.001f * (__popc(A_i_changed) - __popc(A_i));
+                // error_thread2 += error_measure(product_new2, C_ij, weight)
+                //               - error_measure(product_old, C_ij, weight);
             }
         }
         __syncthreads();
     }
     if(i < height) {
         const error_t error_warp = warpReduceSum(error_thread);
+        // const error_t error_warp2 = warpReduceSum(error_thread2);
         // Thread with warpLane==0 now has total error of warp
 
         // Thread 0 checks if new low has been found and applies if necessary
         if (warpLane == 0) {
         // if (i < height && warpLane == 0) {
-            // Metropolis–Hastings algorithm
-            if (metro(state, error_warp, temperature, width)) {
-                A[i] = A_i_changed;
-                atomicAdd(global_error, error_warp);
-            }
+            // if(error_warp < error_warp2) {
+                // Metropolis–Hastings algorithm
+                if (metro(state, error_warp, temperature, width)) {
+                    A[i] = A_i_changed;
+                    atomicAdd(global_error, error_warp);
+                }
+            // } else {
+            //     // Metropolis–Hastings algorithm
+            //     if (metro(state, error_warp2, temperature, width)) {
+            //         A[i] = A_i_changed2;
+            //         atomicAdd(global_error, error_warp2);
+            //     }
+            // }
         }
     }
 }
@@ -246,7 +267,7 @@ vectorMatrixMultCompareColWarpShared(const bit_factor_t * __restrict__ A,
                                      const float temperature,
                                      const float flipManyChance,
                                      const uint32_t flipManyDepth,
-                                     const int weight)
+                                     const error_t weight)
 {
     __shared__ bit_factor_t A_block[32*WARPSPERBLOCK];
     // __shared__ bit_factor_t B_block[32];
@@ -267,16 +288,19 @@ vectorMatrixMultCompareColWarpShared(const bit_factor_t * __restrict__ A,
 
     const bit_factor_t B_j = j < width ? B[j] : 0;
     bit_factor_t B_j_changed = 0;
+    // bit_factor_t B_j_changed2 = 0;
     // if (warpLane == 0 && j < width) {
     if (j < width) {
         state = get_initial_fast_kiss_state32(seed + warpId);
 
         B_j_changed = B_j ^ get_flip_mask(factorDim, state, flipManyChance, flipManyDepth);
+        // B_j_changed2 = B_j ^ get_flip_mask(factorDim, state, flipManyChance, flipManyDepth);
         // B_j_changed = get_flip_mask_many(factorDim, state, flipManyDepth);
     }
     // B_j_changed = __shfl_sync(FULLMASK, B_j_changed, 0);
     
     error_t error_thread = 0;
+    // error_t error_thread2 = 0;
     const index_t vecLane = warpLane;
     const index_t col_in_tile = j % 32;
     const index_t colFirst = j / 32 * 32;
@@ -285,12 +309,14 @@ vectorMatrixMultCompareColWarpShared(const bit_factor_t * __restrict__ A,
         A_block[threadIdx.x] = (i < height) ? A[i] : 0;
         const index_t vecRow = i / 32;
         const index_t vecFirst = vecRow * padded_width + colFirst;
-        C_block[threadIdx.x] = C[vecFirst + warpLane];
+        C_block[threadIdx.x] = (vecRow < SDIV(height,32)) ? C[vecFirst + warpLane] : 0;
         __syncthreads();
 
         if (j < width) {
             #pragma unroll
             for(index_t w = 0; w < WARPSPERBLOCK; ++w) {
+                // const uint32_t dropout = fast_kiss32(state) | fast_kiss32(state) | fast_kiss32(state) & (1 << warpLane);
+                // const bit_factor_t A_i = dropout ? A_block[w*warpSize + warpLane] : 0;
                 const bit_factor_t A_i = A_block[w*warpSize + warpLane];
                 const int C_ij = (C_block[w*warpSize + col_in_tile] >> vecLane) & 1;
 
@@ -299,6 +325,7 @@ vectorMatrixMultCompareColWarpShared(const bit_factor_t * __restrict__ A,
                 // int C_ij = (C[vecFirst + col_in_tile] >> vecLane) & 1;
 
                 const int product_new = (A_i & B_j_changed) ? 1 : 0;
+                // const int product_new2 = (A_i & B_j_changed2) ? 1 : 0;
                 const int product_old = (A_i & B_j        ) ? 1 : 0;
 
                 // const float count_new = __popc(A_i & B_j_changed);
@@ -307,12 +334,16 @@ vectorMatrixMultCompareColWarpShared(const bit_factor_t * __restrict__ A,
                 // if (row < height)
                 error_thread += error_measure(product_new, C_ij, weight)
                               - error_measure(product_old, C_ij, weight);
+                // error_thread += 0.001f * (__popc(B_j_changed) - __popc(B_j));
+                // error_thread2 += error_measure(product_new2, C_ij, weight)
+                //               - error_measure(product_old, C_ij, weight);
             }
         }
         __syncthreads();
     }
     if (j < width) {
         const error_t error_warp = warpReduceSum(error_thread);
+        // const error_t error_warp2 = warpReduceSum(error_thread2);
         // Thread with warpLane==0 now has total error of warp
         // if (warpLane == 0) {
         //     const bool pred = (j < width) && metro(state, error_warp, temperature);
@@ -333,11 +364,19 @@ vectorMatrixMultCompareColWarpShared(const bit_factor_t * __restrict__ A,
         // Thread 0 checks if new low has been found and applies if necessary
         if (warpLane == 0) {
         // if (j < width && warpLane == 0) {
-            // Metropolis–Hastings algorithm
-            if (metro(state, error_warp, temperature, height)) {
-                B[j] = B_j_changed;
-                atomicAdd(global_error, error_warp);
-            }
+            // if(error_warp < error_warp2) {
+                // Metropolis–Hastings algorithm
+                if (metro(state, error_warp, temperature, height)) {
+                    B[j] = B_j_changed;
+                    atomicAdd(global_error, error_warp);
+                }
+            // } else {
+            //     // Metropolis–Hastings algorithm
+            //     if (metro(state, error_warp2, temperature, height)) {
+            //         B[j] = B_j_changed2;
+            //         atomicAdd(global_error, error_warp2);
+            //     }
+            // }
         }
     }
 }
